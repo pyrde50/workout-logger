be16824b32408cc232a617b763fb29dfd90045ce
Sami Kuikka <sami.kuikka@aalto.fi>
2022-10-11 12:59:57 +0300

    login connected to backend

--- src/services/login.js
+++ src/services/login.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:4005/api/login';
+const baseUrl = 'https://workout-logger-backend.onrender.com/api/login';
 
 const login = async credentials => {
     const response = await axios.post(baseUrl, credentials);
-    console.log(response);
     return response.data;
 }
 
