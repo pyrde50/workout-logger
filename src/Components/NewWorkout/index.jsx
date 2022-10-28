4466204f027710428748eb31873f7e96680ec4a2
Pyry Pohjalainen <pyry@MacBook-Pro.local>
2022-10-28 15:51:08 +0300

    add new line finished

--- src/Components/NewWorkout/index.jsx
+++ src/Components/NewWorkout/index.jsx
@@ -3,47 +3,102 @@ import CustomTextField from '../TextField';
 import { Button } from '..';
 import './styles.css';
 
-const NewWorkout = () => {
+const NewWorkout = ({ lines, setLines }) => {
+  const changeData = (index, value) => {
+    const line = lines[index];
+    const newLine = {
+      ...line,
+      ...value,
+    };
+    setLines(
+      lines.map((item, i) => {
+        if (index !== i) {
+          return item;
+        } else {
+          return newLine;
+        }
+      }),
+    );
+  };
   return (
     <div style={{ width: '100%' }}>
       <h2>Add new Workout</h2>
-      <div className="WorkoutBackgroundContainer">
-        <div className="WorkoutInput">
-          <h4>Workout</h4>
-          <CustomTextField width={'85%'} />
-        </div>
-        <div className="WorkoutInput">
-          <h4>Reps</h4>
-          <CustomTextField width={'85%'} />
-        </div>
-        <div className="WorkoutInput">
-          <h4>Amount</h4>
-          <CustomTextField width={'85%'} />
-        </div>
-        <div className="WorkoutInput">
-          <h4>Weight</h4>
-          <div
-            style={{
-              'flex-direction': 'row',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <CustomTextField width={'85%'} />
-            KG
+      {lines.map((item, index) => (
+        <div className="WorkoutBackgroundContainer" key={index}>
+          <div className="WorkoutInput">
+            <h4>Workout</h4>
+            <CustomTextField
+              width={'85%'}
+              value={item.name}
+              onChange={(value) => changeData(index, { name: value })}
+            />
           </div>
+          <div className="WorkoutInput">
+            <h4>Reps</h4>
+            <CustomTextField
+              width={'85%'}
+              value={item.reps}
+              onChange={(value) => changeData(index, { reps: value })}
+            />
+          </div>
+          <div className="WorkoutInput">
+            <h4>Amount</h4>
+            <CustomTextField
+              width={'85%'}
+              value={item.amount}
+              onChange={(value) => changeData(index, { amount: value })}
+            />
+          </div>
+          <div className="WorkoutInput">
+            <h4>Weight</h4>
+            <div
+              style={{
+                'flex-direction': 'row',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <CustomTextField
+                width={'85%'}
+                value={item.weight}
+                onChange={(value) => changeData(index, { weight: value })}
+              />
+              KG
+            </div>
+          </div>
+          <div className="WorkoutInput">
+            <h4>Date</h4>
+            <CustomTextField
+              width={'85%'}
+              value={item.date}
+              onChange={(value) => changeData(index, { date: value })}
+            />
+          </div>
+          {index === 0 ? (
+            <div className="WorkoutInput">
+              <Button text={'SUMBIT'} width={'100%'} height={'40%'} />
+              <div style={{ height: 10 }} />
+              <Button
+                text={'ADD NEW LINE'}
+                width={'100%'}
+                height={'40%'}
+                onClick={() =>
+                  setLines(
+                    lines.concat({
+                      name: '',
+                      reps: 0,
+                      amount: 0,
+                      weight: 0,
+                      date: new Date(),
+                    }),
+                  )
+                }
+              />
+            </div>
+          ) : null}
         </div>
-        <div className="WorkoutInput">
-          <h4>Date</h4>
-          <CustomTextField width={'85%'} />
-        </div>
-        <div className="WorkoutInput">
-          <Button text={'SUMBIT'} width={'100%'} height={'40%'} />
-          <div style={{ height: 10 }} />
-          <Button text={'ADD NEW LINE'} width={'100%'} height={'40%'} />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/Screens/AddWorkout/index.jsx
+++ src/Screens/AddWorkout/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '../../Components';
 import NewWorkout from '../../Components/NewWorkout';
 
 const AddWorkout = () => {
+  const [lines, setLines] = useState([
+    { name: 'aaaaa', reps: 0, amount: 0, weight: 0, date: new Date() },
+  ]);
   return (
     <NavigationContainer>
-      <NewWorkout />
+      <NewWorkout lines={lines} setLines={setLines} />
     </NavigationContainer>
   );
 };
