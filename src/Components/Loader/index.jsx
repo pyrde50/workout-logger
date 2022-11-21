91fdd0d3a7da2d88dbb101c05d9a399ca3aead73
Pyry <pyry.pohjalainen@aalto.fi>
2022-11-21 21:40:19 +0200

    Updates

--- src/Components/Loader/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
-
-const Loader = ({ width, height }) => (
-  <ReactLoading type={'bars'} color={'#70C3FF'} height={height} width={width} />
-);
-
-export default Loader;
--- src/colors.js
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  white: {
-    white: '#FFFFFF',
-  },
-});
-
-export default theme;
